065178f4b9f2cd29b9f3fb69b1621bda9c15801a
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-05-30 18:19:57 +0530

    Update private.py

--- DaisyXMusic/modules/private.py
+++ DaisyXMusic/modules/private.py
@@ -33,6 +33,9 @@ async def start(_, message: Message):
                         "➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
                 [
                     InlineKeyboardButton(
+                        "🧑🏻‍💻Maintained By", url=f"https://t.me/Surv_ivor")],
+                [
+                    InlineKeyboardButton(
                         "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
                 ]
             ]
