8d988946b8a33b6fb8906550cf37d5b608a4c776
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-05-30 17:04:44 +0530

    Update song.py

--- DaisyXMusic/modules/song.py
+++ DaisyXMusic/modules/song.py
@@ -50,7 +50,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔎 `Finding the song...`")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -63,20 +63,20 @@ def song(client, message):
         open(thumb_name, "wb").write(thumb.content)
 
         duration = results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
 
     except Exception as e:
         m.edit("❌ Found Nothing.\n\nTry another keywork or maybe spell it properly.")
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit("`Downloading the song`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by **"
+        rep = f"🎶 **Title:** [{title[:35]}]({link})\n⏳ **Duration:** {duration}\n👀 **Views:** {views}\n**🎵 Uploaded by @GraceMusicBot **"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
